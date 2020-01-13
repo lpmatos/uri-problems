29f880a040ffadf8483a8d3a05481c83dbcddafb
lpmatos <lpmatos@stefanini.com>
2020-01-13 17:46:29 -0300

    Adding ruby URI 1001 script

--- ruby/app/code/1001.rb
+++ ruby/app/code/1001.rb
@@ -1,6 +1,13 @@
 #!/usr/bin/ruby
 
-first = (gets.strip).to_i
-last = (gets.strip).to_i
+def soma(first, last)
+    if first.kind_of? Integer and last.kind_of? Integer
+        return first + last
+    end
+end
 
-puts "X = #{first + last}"
+if __FILE__ == $0
+    first = (gets.strip).to_i
+    last = (gets.strip).to_i
+    puts "X = #{soma(first, last)}"
+end 
