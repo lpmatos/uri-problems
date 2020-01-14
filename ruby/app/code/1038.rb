9c3c47ae199698a4cd0edc2fb140c62026e001d1
lpmatos <lpmatos@stefanini.com>
2020-01-13 23:57:45 -0300

    Adding scripts uri 1038 ruby

--- ruby/app/code/1038.rb
+++ ruby/app/code/1038.rb
@@ -6,6 +6,7 @@
 
 $codes = (%w(1 2 3 4 5)).map{ |value| value.to_i } # Usando map para converter todo array para inteiro
 $prices = (%w(4 4.50 5 2 1.5)).map(&:to_f) # Usando map para converter todo array para float
+$table = $codes.zip($prices)
 
 # =============================================================================
 # MAIN
@@ -17,4 +18,5 @@ if __FILE__ == $0
     puts "#{valores}"
     puts "#{$codes}"
     puts "#{$prices}"
+    puts "#{$table}"
 end
