774eab855c20ea57aed70abff899f4e732d716ac
lpmatos <lpmatos@stefanini.com>
2020-01-14 19:22:05 -0300

    Adding ruby URI 1168 script

--- ruby/app/code/1168.rb
+++ ruby/app/code/1168.rb
@@ -1,21 +1,22 @@
 #!/usr/bin/ruby
 
 # =============================================================================
-# FUNCTIONS
+# CONSTANTS
 # =============================================================================
 
-def filter(array)
-    return ((array.map{ |valor| valor if valor % 2 == 0 }).compact).size
-end
+$LEDS = (%w(2 5 5 4 5 6 3 7 6 6)).map{ |value| value.to_i } 
+
+# =============================================================================
+# FUNCTIONS
+# =============================================================================
 
 # =============================================================================
 # MAIN
 # =============================================================================
 
 if __FILE__ == $0
-    values = Array.new(5)
-    values.each_with_index do |value, index|
-        values[index] = (gets.strip).to_i
+    count = (gets.strip).to_i - 1
+    for elemento in 0..count
+        puts "#{$LEDS}"
     end
-    puts "#{filter(values)} valores pares"
 end
