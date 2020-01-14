ca82681e9597ae8fbb8df4cd81b3004e2057b79d
lpmatos <lpmatos@stefanini.com>
2020-01-14 20:07:23 -0300

    Adding ruby URI 1168 script

--- ruby/app/code/1168.rb
+++ ruby/app/code/1168.rb
@@ -15,10 +15,17 @@ $LEDS = (%w(2 5 5 4 5 6 3 7 6 6)).map{ |value| value.to_i }
 # =============================================================================
 
 if __FILE__ == $0
+    # GETTING QUANTIDADE
     quantidade = (gets.strip).to_i - 1
+    # LOOP QUANTIDADE
     for elemento in 0..quantidade
+        # GETTING NUMBER
         number = (gets.strip).split("")
-        final = number.zip($LEDS)
-        puts "#{final}"
+        # LOOP
+        number.each do |value|
+            number[value.to_i] = LEDS[value.to_i]
+        end
+        # SHOW
+        puts "#{number}"
     end
 end
