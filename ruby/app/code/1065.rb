d1892c5955814072007e60c1b59ab5c6b1777523
lpmatos <lpmatos@stefanini.com>
2020-01-14 19:01:19 -0300

    Adding ruby URI 1065 script

--- ruby/app/code/1065.rb
+++ ruby/app/code/1065.rb
@@ -14,8 +14,8 @@ end
 
 if __FILE__ == $0
     values = Array.new(5)
-    for elemento in values
-        values[elemento] = (gets.strip).to_i
+    values.each_with_index do |value, index|
+        values[index] = (gets.strip).to_i
     end
     puts "#{values}"
     puts "#{filter(values)}"
