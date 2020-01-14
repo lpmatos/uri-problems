e8c0a0e96081f003cad9ba6c3dcf7934b43ee963
lpmatos <lpmatos@stefanini.com>
2020-01-14 00:07:52 -0300

    Adding scripts uri 1038 ruby

--- ruby/app/code/1038.rb
+++ ruby/app/code/1038.rb
@@ -27,6 +27,7 @@ if __FILE__ == $0
     if valores.size == 2
         first = (valores[0]).to_i
         count = (valores[1]).to_i
-        puts "#{$table[first] * count}"
+        puts "#{$table}"
+        puts "#{count}"
     end
 end
