8dd798b1936dda477cd93db26ea4ad0952a596b8
lpmatos <lpmatos@stefanini.com>
2020-01-14 00:28:20 -0300

    Adding scripts uri 1038 ruby

--- ruby/app/code/1038.rb
+++ ruby/app/code/1038.rb
@@ -30,6 +30,6 @@ if __FILE__ == $0
         count = (valores[1]).to_i
         first = $table[first]
         first = first[1]
-        puts sprintf "Total: R$ %.2f\n" % (prod(final, count))
+        puts sprintf "Total: R$ %.2f\n" % (prod(first, count))
     end
 end
