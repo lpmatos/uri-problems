b9a2f76534978a2c3e7716eb401e4f6fe3c0429d
lpmatos <lpmatos@stefanini.com>
2020-01-14 19:50:25 -0300

    Adding ruby URI 1168 script

--- ruby/app/code/1168.rb
+++ ruby/app/code/1168.rb
@@ -17,7 +17,7 @@ $LEDS = (%w(2 5 5 4 5 6 3 7 6 6)).map{ |value| value.to_i }
 if __FILE__ == $0
     count = (gets.strip).to_i - 1
     for elemento in 0..count
-        number = (gets.strip).to_i
+        number = (gets.strip)
         for value in number
             puts "#{value}"
         end
