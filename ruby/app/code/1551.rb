7af3ee8a978f44d87b60fc869fb1d0127e84616b
lpmatos <lpmatos@stefanini.com>
2020-01-15 17:45:42 -0300

    Adding URI solution in ruby to problem 1551

--- ruby/app/code/1551.rb
+++ ruby/app/code/1551.rb
@@ -7,15 +7,26 @@
 $alfabeto = (%w(a b c d e f g h i j k l m n o p q r s t u v x z)).map{ |value| value.to_s }
 
 # =============================================================================
+
+def exit(value)
+    if value.length < 3 and value.length > 1000
+        exit
+    end
+end
+
+# =============================================================================
 # MAIN
 # =============================================================================
 
 if __FILE__ == $0
     quantidade = (gets.strip).to_i - 1
     for elemento in 0..quantidade
-        frase = (gets.to_s).gsub(" ", "")
-        frase = frase.gsub(",", "")
-        puts "#{$alfabeto}"
-        puts "#{frase}"
+        frase = (gets.to_s).gsub(",", "") # O método gsub executa o replace em todas as instâncias, o que é diferente do método sub que executa apenas na primeira ocorrência
+        exit(frase)
+        frase = frase.gsub(" ", "")
+        words = frase.split("")
+        puts "#{$alfabeto}\n"
+        puts "#{frase}\n"
+        puts "#{words}\n"
     end
 end
