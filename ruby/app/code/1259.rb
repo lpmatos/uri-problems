b910b59dd1948fd57a656c450076ff033618062a
lpmatos <lpmatos@stefanini.com>
2020-01-15 18:31:50 -0300

    Adding URI solution in ruby to problem 1259

--- ruby/app/code/1259.rb
+++ ruby/app/code/1259.rb
@@ -1,5 +1,54 @@
 #!/usr/bin/ruby
 
+# =============================================================================
+# FUNCTIONS
+# =============================================================================
+
+def count_par(array)
+    if array.kind_of? Array
+        return array.count { |value| value % 2 == 0 }
+    end
+end
+
+# =============================================================================
+
+def count_impar(array)
+    if array.kind_of? Array
+        return array.count { |value| value % 2 != 0 }
+    end
+end
+
+# =============================================================================
+
+def sort(array)
+    array = array.sort
+    par = Array.new(count_par(array))
+    impar = Array.new(count_par(array))
+    for valor in array
+        if par(valor.to_i)
+            par.push(valor)
+        else
+            impar.push(valor)
+        end
+    end
+    return par.concat(impar)
+end
+
+# =============================================================================
+
+def par(value)
+    if value.kind_of? Integer
+        if value % 2 == 0
+            return true
+        else
+            return false
+    end
+end
+
+# =============================================================================
+# MAIN
+# =============================================================================
+
 if __FILE__ == $0
     quantidade = (gets.strip).to_i
     array = Array.new(quantidade)
@@ -7,4 +56,5 @@ if __FILE__ == $0
         array[index] = (gets.strip).to_i
     end
     puts "#{array}"
+    puts "#{sort(array)}"
 end
