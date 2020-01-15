1b9ec3ddffc558ffc14d4f93dc7bdaee1a1d5f92
lpmatos <lpmatos@stefanini.com>
2020-01-15 18:08:17 -0300

    Adding URI solution in ruby to problem 1259

---
+++ ruby/app/code/1259.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/ruby
+
+if __FILE__ == $0
+    quantidade = (gets.strip).to_i - 1
+    array = Array.new(quantidade + 1)
+    for elemento in 0..quantidade
+        value = (gets.strip).to_i
+        array.each_with_index do | value, index |
+            array[index] = value
+        end
+        puts "#{array}"
+    end
+end
