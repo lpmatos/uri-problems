f047fe52fa6c8959244c318d979c01ba7820ee69
lpmatos <lpmatos@stefanini.com>
2020-01-18 16:57:04 -0300

    Adding 1241.rb solution URI in ruby

--- solutions/ruby/code/1241.rb
+++ solutions/ruby/code/1241.rb
@@ -7,14 +7,17 @@
 if __FILE__ == $0
     quantidade = (gets.strip).to_i - 1
     for elemento in 0..quantidade
-        first, last = gets.strip, gets.strip
-        if (first.length > 0 and last.length > 0) and (first.length <= 1000 and last.length)
-            tamanho = last.length
-            corte = (first.length - last.length).abs
-            atual = first[corte, first.length - 1]
-            puts atual
-        else
-            exit
+        valores = ((STDIN.gets).strip).split(" ")
+        if valores.size == 2
+            first, last = valores[0].to_i), valores[1].to_i
+            if (first.length > 0 and last.length > 0) and (first.length <= 1000 and last.length <= 1000)
+                tamanho = last.length
+                diferenca = (first.length - tamanho).abs
+                atual = first[diferenca..(first.length - 1)]
+                puts atual
+            else
+                exit
+            end
         end
     end
 end
