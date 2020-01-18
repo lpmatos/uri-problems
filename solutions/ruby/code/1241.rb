ca06753935d9821c5a25745893fb0a139deb7437
lpmatos <lpmatos@stefanini.com>
2020-01-18 17:06:15 -0300

    Adding 1241.rb solution URI in ruby

--- solutions/ruby/code/1241.rb
+++ solutions/ruby/code/1241.rb
@@ -8,20 +8,10 @@ if __FILE__ == $0
     quantidade = (gets.strip).to_i - 1
     for elemento in 0..quantidade
         valores = ((STDIN.gets).strip).split(" ")
-        if valores.size == 2
-            first, last = valores[0].to_s, valores[1].to_s
-            if (first.length > 0 and last.length > 0) and (first.length <= 1000 and last.length <= 1000)
-                tamanho = last.length
-                diferenca = (first.length - tamanho).abs
-                atual = first[diferenca..(first.length - 1)]
-                if atual == last
-                    puts "encaixa"
-                else
-                    puts "nao encaixa"
-                end
-            else
-                exit
-            end
+        break if valores.size != 2
+        first, last = valores[0].to_s, valores[1].to_s
+        if (first.length > 0 and last.length > 0) and (first.length <= 1000 and last.length <= 1000)
+            first[(first.length - last.length).abs..(first.length - 1)] == last ? puts "encaixa" : puts "nao encaixa"
         end
     end
 end
