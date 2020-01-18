f33c7ea9b640924b641291bc987fa31117b2663d
lpmatos <lpmatos@stefanini.com>
2020-01-18 17:08:08 -0300

    Adding 1241.rb solution URI in ruby

--- solutions/ruby/code/1241.rb
+++ solutions/ruby/code/1241.rb
@@ -11,7 +11,7 @@ if __FILE__ == $0
         break if valores.size != 2
         first, last = valores[0].to_s, valores[1].to_s
         if (first.length > 0 and last.length > 0) and (first.length <= 1000 and last.length <= 1000)
-            first[(first.length - last.length).abs..(first.length - 1)] == last ? puts "encaixa" : puts "nao encaixa"
+            if first[(first.length - last.length).abs..(first.length - 1)] == last then puts "encaixa" else puts "nao encaixa" end
         end
     end
 end
